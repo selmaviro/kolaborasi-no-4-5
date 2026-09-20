428bcf022b6b0fde9b91f51119c90fd9ae7ffcc0
{selmaviro} <{selmaviro@gmail.com}>
2026-09-20 04:53:41 +0000

    selmaviro: Menambahkan fungsi input data

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # Program Utama Kelompok
+
+def input_data():
+    nama = input("Masukkan Nama: ")
+    nilai = input("Masukkan Nilai: ")
+    return {"nama": nama, "nilai": nilai}
+
+if __name__ == "__main__":
+    data = input_data()
+    print("Data tersimpan:", data)
